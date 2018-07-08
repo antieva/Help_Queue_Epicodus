0a9ab2a955feb5843a343b302064fdbba9638984
Eva Antipina <eva.antipina@gmail.com>
2018-07-08 12:09:41 -0700

    Add TicketDetail component.

---
+++ src/components/TicketDetail.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TicketDetail(props){
+  return (
+    <div>
+     <hr/>
+     <h2>{props.selectedTicket.names} - {props.selectedTicket.location}</h2>
+     <h3>Submitted {props.selectedTicket.formattedWaitTime} ago</h3>
+     <h4 className='issue'><em>{props.selectedTicket.issue}</em></h4>
+     <hr/>
+   </div>
+  );
+}
+
+TicketDetail.propTypes = {
+  selectedTicket: PropTypes.object
+};
+
+export default TicketDetail;
